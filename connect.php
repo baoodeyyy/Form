56e7b8742e90020f5dd99fe717ad234476036df3
baoodeyyy <86925440+baoodeyyy@users.noreply.github.com>
2023-01-16 11:47:38 +0700

    push lan 2

--- connect.php
+++ connect.php
@@ -17,8 +17,14 @@ $bookAddress2 = $_POST['bookAddress2'];
 $bookCity = $_POST['bookCity'];
 $bookState = $_POST['bookState'];
 $bookPostal = $_POST['bookPostal'];
-$roomType = $_POST['roomType'];
-$smoking = $_POST['smoking'];
+if (isset ( $_POST ['roomType'] )) {
+    $firstName = $_POST ['roomType'];
+}
+if (isset ( $_POST ['smoking'] )) {
+    $firstName = $_POST ['smoking'];
+}
+//$roomType = $_POST['roomType'];
+//$smoking = $_POST['smoking'];
 $ofGuest = $_POST['ofGuest'];
 $note = $_POST['note'];
 
@@ -29,7 +35,7 @@ if ($conn -> connect_error){
     die('Connection Failed :' .$conn->connect_error);
 }else {
     $stmt = $conn->prepare("inset into reg(firstName,lastName,email,phoneNumber,address1,address2,city,state,postal,checkInDate,checkOutDate,bookAddress1,bookAddress2,bookCity,bookState,bookPostal,roomType,smoking,ofGuest,note) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("sssissssissssssiiss",$firstName,$lastName,$email,$phoneNumber,$address1,$address2,$city,$state,$postal,$checkInDate,$checkOutDate,$bookAddress1,$bookAddress2,$bookCity,$bookState,$bookPostal,$roomType,$smoking,$ofGuest,$note);
+    $stmt->bind_param("sssissssissssssisbbs",$firstName,$lastName,$email,$phoneNumber,$address1,$address2,$city,$state,$postal,$checkInDate,$checkOutDate,$bookAddress1,$bookAddress2,$bookCity,$bookState,$bookPostal,$roomType,$smoking,$ofGuest,$note);
     $stmt->execute();
     echo "reg successfully";
     $stmt->close();
