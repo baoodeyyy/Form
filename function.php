ebd19b46aa0188e6ef52f155873d6759ae105983
baoodeyyy <86925440+baoodeyyy@users.noreply.github.com>
2023-02-09 14:03:47 +0700

    function

--- function.php
+++ function.php
@@ -1,1 +1,16 @@
 <?php
+$messageArr = [
+  1 => "Thành công",
+    2 => ".."
+];
+function redirect($path){
+    header("Location: ".$path);
+    exit;
+}
+function get_message($code){
+    global $messageArr;
+    if (array_key_exists($code, $messageArr)){
+        return $messageArr[$code];
+    }
+    return false;
+}
